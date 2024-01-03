2d2ecfe989764c8c78b26f0cef398c2167ffef41
ameyht <ameyb@heaptrace.net>
2024-01-03 16:04:51 +0530

    height issue solved in Main

--- userwidget2/src/components/Main.jsx
+++ userwidget2/src/components/Main.jsx
@@ -5,7 +5,7 @@ import UserWidgetComponent from "./UserWidgetComponent";
 
 const Main = () => {
   return (
-    <div className="min-h-16 h-auto md:min-h-16 md:h-auto w-[100%] border border-blue-300 bg-gray-100 md:w-[84%]">
+    <div className="h-[100vh] w-[100%] border border-blue-300 bg-gray-100 md:w-[84%]">
       <Header />
       <Users />
       <UserWidgetComponent />
