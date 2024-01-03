108b31645dfeb2474aabb61edec5f19eb73ef148
ameyht <ameyb@heaptrace.net>
2024-01-03 18:11:04 +0530

    Change in Button in User Component

--- userwidget2/src/components/Users.jsx
+++ userwidget2/src/components/Users.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./css/Users.css";
+import { CgMenuGridO } from "react-icons/cg";
 
 const Users = () => {
   return (
@@ -36,7 +37,7 @@ const Users = () => {
             </svg>
           </button>
           <button className="xe ">
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               width="25"
               height="25"
@@ -45,7 +46,8 @@ const Users = () => {
               viewBox="0 0 16 16"
             >
               <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
-            </svg>
+            </svg> */}
+            <CgMenuGridO className="h-[1.5rem] w-[1.5rem]" />
           </button>
         </div>
         {/* Add user button */}
